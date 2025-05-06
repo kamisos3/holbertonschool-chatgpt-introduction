bf3ea89016a7acc0513b86b22432b3fb55c103ce
kamisos3 <kamilasostre11@gmail.com>
2025-05-06 14:00:39 -0400

    Fixing factorial code output

---
+++ factorial.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+num = int(sys.argv) != 2:
+if num < 0:
+    raise ValueError("Number must be non-negative.")
+    except ValueError as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+
+    f = factorial(num)
+    print(f)
