4baa88588784df61aa729ec51bbdd628c054edd2
kamisos3 <kamilasostre11@gmail.com>
2025-05-06 17:37:32 -0400

    Making file executable

---
+++ debugging/factorial_correction.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./factorial.py <non-negative-integer>")
+        sys.exit(1)
+
+        try:
+            num = int(sys.argv[1])
+            if num < 0:
+                raise ValueError("Number must be non-negative.")
+        except ValueError as e:
+          print(f"Error: {e}")
+          sys.exit(1)
+
+        f = factorial(num)
+        print(f)
