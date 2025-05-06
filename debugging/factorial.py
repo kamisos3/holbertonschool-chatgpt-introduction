da401e6b28d61898ac71a0d6e638342a347264da
kamisos3 <kamilasostre11@gmail.com>
2025-05-06 17:21:37 -0400

    Fixing errors in factorial code

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,7 +5,21 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
+        n -= 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./factorial.py <non-negative-integer>")
+        sys.exit(1)
+
+        try:
+            num = int(sys.argv[1])
+            if num < 0:
+                raise ValueError("Number must be non-negative.")
+        except ValueError as e:
+          print(f"Error: {e}")
+          sys.exit(1)
+
+        f = factorial(num)
+        print(f)
