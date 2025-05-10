2a1e36d6ca06e1f7e7d4c83a19bf7d1abb8d9459
kamisos3 <kamilasostre11@gmail.com>
2025-05-10 15:33:11 -0400

    Making file executable

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import sys
 
 def factorial(n):
@@ -10,16 +10,14 @@ def factorial(n):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: ./factorial.py <non-negative-integer>")
+        print("Usage: ./factorial.py <non-negative integer>")
         sys.exit(1)
 
-        try:
-            num = int(sys.argv[1])
-            if num < 0:
-                raise ValueError("Number must be non-negative.")
-        except ValueError as e:
-          print(f"Error: {e}")
-          sys.exit(1)
-
-        f = factorial(num)
-        print(f)
+    try:
+        num = int(sys.argv[1])
+        if num < 0:
+            raise ValueError("Negative number")
+        print(factorial(num))
+ except ValueError:
+        print("Error: Input must be a non-negative integer.")
+        sys.exit(1)
