fc36e0cf6ecbbcde0611a610067a7660e04174f0
kamisos3 <kamilasostre11@gmail.com>
2025-05-12 14:23:38 -0400

    Debugging a function that outputs a factorial number

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -8,14 +8,14 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
-    
-    if __name__ == "__main__":
-        if len(sys.argv) != 2:
-            print("Usage: {} <non-negative integer>".format(sys.argv[0]))
-            sys.exit(1)
-        try:
-            num = int(sys.argv[1])
-            print(factorial(num))
-        except ValueError as e:
-            print("Error:", e)
-            sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <non-negative integer>".format(sys.argv[0]))
+        sys.exit(1)
+    try:
+        num = int(sys.argv[1])
+        print(factorial(num))
+    except ValueError as e:
+        print("Error:", e)
+        sys.exit(1)
